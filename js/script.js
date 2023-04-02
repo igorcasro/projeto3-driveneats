7f6c000b2c21bba72dde37efac3bbe4c43290100
Igor Castro <igorpfcastro@gmail.com>
2023-04-02 19:46:29 -0300

    ended basic implementation with everything working

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let drinkName = '';
 let drinkValue = '';
 let dessertName = '';
 let dessertValue = '';
+let name = '';
+let address = '';
 
 function selectItem (parentClass, itemClass) {
     // Verify if already has a selected card
@@ -43,15 +45,46 @@ function checkIfAllActive(food, drink, dessert) {
         const selectedButton = document.querySelector(".selected-button");
         selectedButton.classList.remove("hidden");
 
-        foodName = checkFood.querySelector('.name');
-        foodValue = checkFood.querySelector('.value');
-        drinkName = checkDrink.querySelector('.name');
-        drinkValue = checkDrink.querySelector('.value');
-        dessertName = checkDessert.querySelector('.name');
-        dessertValue = checkDessert.querySelector('.value');
+        foodName = checkFood.querySelector('.name').innerHTML;
+        foodValue = checkFood.querySelector('.value').innerHTML;
+        drinkName = checkDrink.querySelector('.name').innerHTML;
+        drinkValue = checkDrink.querySelector('.value').innerHTML;
+        dessertName = checkDessert.querySelector('.name').innerHTML;
+        dessertValue = checkDessert.querySelector('.value').innerHTML;
     }    
+}
+
+function parseValues() {
+    const parsedFoodValue = Number(foodValue.substring(2, foodValue.length - 1).replace(",", "."));
+    const parsedDrinkValue = Number(drinkValue.substring(2, drinkValue.length - 1).replace(",", "."));
+    const parsedDessertValue = Number(dessertValue.substring(2, dessertValue.length - 1).replace(",", "."));
+
+    return (parsedFoodValue + parsedDrinkValue + parsedDessertValue).toFixed(2);
 }
 
 function sendMessage() {
 
+    name = prompt("Insira seu nome, por favor:");
+    address = prompt("Por último, seu endereço:");
+
+    const totalValue = parseValues();
+
+    const cellphoneNumber = 5519993027135;
+
+    const textMessage = 'Olá, gostaria de fazer o pedido:';
+    
+    const foodMessage = '- Prato: ' + foodName;
+    const drinkMessage = '- Bebida: ' + drinkName;
+    const dessertMessage = '- Sobremesa: ' + dessertName;
+    const totalOrder = 'Total: ' + totalValue.replace(".", ",");
+    const endName = 'Nome: ' + name;
+    const endAddress = 'Endereço: ' + address;
+
+    let fullMessage = textMessage + '\n' + foodMessage + '\n' + drinkMessage + '\n' + dessertMessage + '\n' + totalOrder + '\n\n' + endName + '\n' + endAddress;
+    
+    const wppMessage = 'https://wa.me/' + cellphoneNumber + '?text=' + encodeURIComponent(fullMessage) ;
+
+    console.log(wppMessage);
+
+    window.open(wppMessage);
 }
